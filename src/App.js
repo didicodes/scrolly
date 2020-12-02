20edebe645ea720fc5f332f5a6055e0017c9ee78
Edidiong Asikpo <edidiongasikpo@Edidiongs-MacBook-Pro.local>
2020-12-02 08:13:11 +0100

    Animate elements on the scroll using the ScrollTrigger plugin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
-import './App.css';
+import { useRef, useEffect } from "react";
+import "./App.css";
 import workout from "./workout.svg";
 import greensocklogo from "./greensocklogo.svg";
 import happy from "./happy.svg";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef(null);
+  useEffect(() => {
+    const element = ref.current;
+    gsap.fromTo(
+      element.querySelector(".first-paragraph"),
+      {
+        opacity: 0,
+        y: -20
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: element.querySelector(".first"),
+          start: "top top",
+          end: "bottom center",
+          scrub: true
+        }
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+    gsap.fromTo(
+      element.querySelector("#gsap-logo"),
+      {
+        opacity: 0,
+        scale: 0.2,
+        y: -20
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        duration: 1,
+        ease: "none",
+        scrollTrigger: {
+          trigger: element.querySelector(".first"),
+          start: "top center",
+          end: "bottom top",
+          scrub: true
+        }
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+    gsap.from(element.querySelector(".line"), {
+      scale: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: element.querySelector(".third"),
+        scrub: true,
+        start: "top bottom",
+        end: "top top"
+      }
+    });
+  }, []);
+
   return (
-   <div className="App">
+    <div className="App" ref={ref}>
       <div className="first">
         <h1>ScrollTrigger</h1>
         <p className="first-paragraph">
@@ -37,4 +102,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
