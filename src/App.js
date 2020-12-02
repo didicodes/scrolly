512bcc1e4994e4788a94e3ea4a910f0e9a60d04e
Edidiong Asikpo <edidiongasikpo@Edidiongs-MacBook-Pro.local>
2020-12-02 07:04:22 +0100

    Added three SVG images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import workout from "./workout.svg";
+import greensocklogo from "./greensocklogo.svg";
+import happy from "./happy.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+   <div className="App">
+      <div className="first">
+        <h1>ScrollTrigger</h1>
+        <p className="first-paragraph">
+          is the coolest Greensock plugin.
+          <span role="img" aria-label="celebrating">
+            🥳
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="logo-main">
+          <img src={workout} id="workout-logo" alt="workout" />
+        </div>
+      </div>
+
+      <div className="second">
+        <div className="logo-main">
+          <img src={greensocklogo} id="gsap-logo" alt="greensocklogo" />
+        </div>
+      </div>
+
+      <div className="third">
+        <p>
+          <span className="line" />
+        </p>
+        <div className="logo-main">
+          <img src={happy} id="happy-logo" alt="happy" />
+        </div>
+      </div>
     </div>
   );
 }
